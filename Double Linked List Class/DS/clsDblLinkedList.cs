1483d479879c9aee5405a0f28c44883791e07a7f
Medhat3760 <Medhat3760@gmail.com>
2025-10-17 21:20:16 +0200

    Added a project 1

---
+++ Double Linked List Class/DS/clsDblLinkedList.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Double_Linked_List_Class.DS
+{
+    public class clsDblLinkedList<T>
+    {
+
+        public class Node
+        {
+
+            public T value;
+            public Node next;
+            public Node prev;
+
+        }
+
+        public Node head = null;
+
+        public void InsertAtBeginning(T value)
+        {
+
+            Node newNode = new Node();
+            newNode.value = value;
+            newNode.next = head;
+            newNode.prev = null;
+
+            if (head != null)
+            {
+                head.prev = newNode;
+            }
+
+            head = newNode;
+
+        }
+
+        public void PrintList()
+        {
+
+            Node current = head;
+
+            while (current != null)
+            {
+
+                Console.Write(current.value + " ");
+
+                current = current.next;
+
+            }
+
+            Console.WriteLine();
+
+        }
+
+        public Node Find(T value)
+        {
+
+            Node current = head;
+
+            while (current != null)
+            {
+
+                if (current.value.Equals(value))
+                {
+                    return current;
+                }
+
+                current = current.next;
+
+            }
+
+            return null;
+
+        }
+
+        public void InsertAfter(Node current, T value)
+        {
+
+            if (current == null) return;
+
+            Node newNode = new Node();
+            newNode.value = value;
+            newNode.next = current.next;
+            newNode.prev = current;
+
+            if (current.next != null)
+            {
+                current.next.prev = newNode;
+            }
+
+            current.next = newNode;
+
+        }
+
+        public void InsertAtEnd(T value)
+        {
+
+            Node newNode = new Node();
+            newNode.value = value;
+            newNode.next = null;
+
+            if (head == null)
+            {
+                newNode.prev = null;
+                head = newNode;
+                return;
+            }
+
+            Node current = head;
+            while (current.next != null)
+            {
+                current = current.next;
+            }
+
+            current.next = newNode;
+            newNode.prev = current;
+
+        }
+
+        public void DeleteNode(ref Node nodeToDelete)
+        {
+
+            if (head == null || nodeToDelete == null)
+            {
+                return;
+            }
+
+            if (head == nodeToDelete)
+            {
+                head = nodeToDelete.next;
+            }
+
+            if (nodeToDelete.next != null)
+            {
+                nodeToDelete.next.prev = nodeToDelete.prev;
+            }
+
+            if (nodeToDelete.prev != null)
+            {
+                nodeToDelete.prev.next = nodeToDelete.next;
+            }
+
+            nodeToDelete = null;
+
+        }
+
+        public void DeleteFirstNode()
+        {
+
+            if (head == null) return;
+
+            head = head.next;
+
+            if(head != null)
+            {
+                head.prev = null;
+            }
+
+        }
+
+        public void DeleteLastNode()
+        {
+            if (head == null) return;
+
+            if(head.next == null)
+            {
+                head = null;
+                return;
+            }
+
+            Node current = head;
+            while(current.next.next != null)
+            {
+                current= current.next;
+            }
+
+            current.next = null;
+
+        }
+
+    }
+}
---
+++ Double Linked List Class/Program.cs
@@ -0,0 +1,57 @@
+﻿using Double_Linked_List_Class.DS;
+namespace Double_Linked_List_Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            clsDblLinkedList<int> myDblLinkedList = new clsDblLinkedList<int>();
+
+            myDblLinkedList.InsertAtBeginning(5);
+            myDblLinkedList.InsertAtBeginning(4);
+            myDblLinkedList.InsertAtBeginning(3);
+            myDblLinkedList.InsertAtBeginning(2);
+            myDblLinkedList.InsertAtBeginning(1);
+
+            Console.WriteLine("\nLinked List Content:");
+            myDblLinkedList.PrintList();
+
+            clsDblLinkedList<int>.Node n1 = myDblLinkedList.Find(2);
+
+            if(n1 != null)
+            {
+                Console.WriteLine("\nNode with value 2 is found :-)");
+            }
+            else
+            {
+                Console.WriteLine("\nNode with value 2 is NOT found :-(");
+            }
+
+            myDblLinkedList.InsertAfter(n1, 500);
+            Console.WriteLine("\nAfter inserting 500 after 2:");
+            myDblLinkedList.PrintList();
+
+            myDblLinkedList.InsertAtEnd(700);
+            Console.WriteLine("\nAfter inserting 700 at end:");
+            myDblLinkedList.PrintList();
+
+            clsDblLinkedList<int>.Node n2 = myDblLinkedList.Find(4);
+
+            myDblLinkedList.DeleteNode(ref n2);
+            Console.WriteLine("\nAfter Deleting 4:");
+            myDblLinkedList.PrintList();
+
+            myDblLinkedList.DeleteFirstNode();
+            Console.WriteLine("\nAfter Delete First Node:");
+            myDblLinkedList.PrintList();
+
+            myDblLinkedList.DeleteLastNode();
+            Console.WriteLine("\nAfter Delete Last Node:");
+            myDblLinkedList.PrintList();
+
+            Console.ReadKey();
+
+        }
+    }
+}

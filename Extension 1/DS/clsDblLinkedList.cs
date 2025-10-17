821caccc4f601872dcb6bf707619c6004df7444e
Medhat3760 <Medhat3760@gmail.com>
2025-10-17 22:35:15 +0200

    Added an extension to Project 1

---
+++ Extension 1/DS/clsDblLinkedList.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Double_Linked_List_Class.DS
+{
+    public class clsDblLinkedList<T>
+    {
+
+        private int _size;
+
+        public class Node
+        {
+
+            public T value;
+            public Node next;
+            public Node prev;
+
+        }
+
+        public Node head = null;
+
+        public void InsertAtBeginning(T value)
+        {
+
+            Node newNode = new Node();
+            newNode.value = value;
+            newNode.next = head;
+            newNode.prev = null;
+
+            if (head != null)
+            {
+                head.prev = newNode;
+            }
+
+            head = newNode;
+
+            _size++;    
+
+        }
+
+        public void PrintList()
+        {
+
+            Node current = head;
+
+            while (current != null)
+            {
+
+                Console.Write(current.value + " ");
+
+                current = current.next;
+
+            }
+
+            Console.WriteLine();
+
+        }
+
+        public Node Find(T value)
+        {
+
+            Node current = head;
+
+            while (current != null)
+            {
+
+                if (current.value.Equals(value))
+                {
+                    return current;
+                }
+
+                current = current.next;
+
+            }
+
+            return null;
+
+        }
+
+        public void InsertAfter(Node current, T value)
+        {
+
+            if (current == null) return;
+
+            Node newNode = new Node();
+            newNode.value = value;
+            newNode.next = current.next;
+            newNode.prev = current;
+
+            if (current.next != null)
+            {
+                current.next.prev = newNode;
+            }
+
+            current.next = newNode;
+
+            _size++;
+
+        }
+
+        public void InsertAtEnd(T value)
+        {
+
+            Node newNode = new Node();
+            newNode.value = value;
+            newNode.next = null;
+
+            if (head == null)
+            {
+                newNode.prev = null;
+                head = newNode;
+                return;
+            }
+
+            Node current = head;
+            while (current.next != null)
+            {
+                current = current.next;
+            }
+
+            current.next = newNode;
+            newNode.prev = current;
+
+            _size++;
+        }
+
+        public void DeleteNode(ref Node nodeToDelete)
+        {
+
+            if (head == null || nodeToDelete == null)
+            {
+                return;
+            }
+
+            if (head == nodeToDelete)
+            {
+                head = nodeToDelete.next;
+            }
+
+            if (nodeToDelete.next != null)
+            {
+                nodeToDelete.next.prev = nodeToDelete.prev;
+            }
+
+            if (nodeToDelete.prev != null)
+            {
+                nodeToDelete.prev.next = nodeToDelete.next;
+            }
+
+            nodeToDelete = null;
+
+            _size--;
+
+        }
+
+        public void DeleteFirstNode()
+        {
+
+            if (head == null) return;
+
+            head = head.next;
+
+            if(head != null)
+            {
+                head.prev = null;
+            }
+
+            _size--;
+
+        }
+
+        public void DeleteLastNode()
+        {
+            if (head == null) return;
+
+            if(head.next == null)
+            {
+                head = null;
+                return;
+            }
+
+            Node current = head;
+            while(current.next.next != null)
+            {
+                current= current.next;
+            }
+
+            current.next = null;
+
+            _size--;
+
+        }
+
+        public int Size
+        {
+            get{ return _size; }
+        }
+
+    }
+}
---
+++ Extension 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using Double_Linked_List_Class.DS;
+namespace Extension_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            clsDblLinkedList<int> myDblLinkedList = new clsDblLinkedList<int>();
+
+            myDblLinkedList.InsertAtBeginning(5);
+            myDblLinkedList.InsertAtBeginning(4);
+            myDblLinkedList.InsertAtBeginning(3);
+            myDblLinkedList.InsertAtBeginning(2);
+            myDblLinkedList.InsertAtBeginning(1);
+
+            Console.WriteLine("\nLinked List Content:");
+            myDblLinkedList.PrintList();
+
+            Console.WriteLine("\nNumber of items in the linked list = " + myDblLinkedList.Size);
+
+            Console.ReadKey();
+
+        }
+    }
+}

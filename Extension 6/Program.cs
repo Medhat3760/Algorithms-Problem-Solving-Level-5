68e6429b892fa651e0be0f4be6ecc0a38173f8d1
Medhat3760 <Medhat3760@gmail.com>
2025-10-19 02:57:38 +0200

    Added an extension to Project 1

---
+++ Extension 6/Program.cs
@@ -0,0 +1,24 @@
+﻿using Double_Linked_List_Class.DS;
+namespace Extension_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            clsDblLinkedList<int> myDblLinkedList = new clsDblLinkedList<int>();
+
+            myDblLinkedList.InsertAtBeginning(5);
+            myDblLinkedList.InsertAtBeginning(4);
+            myDblLinkedList.InsertAtBeginning(3);
+            myDblLinkedList.InsertAtBeginning(2);
+            myDblLinkedList.InsertAtBeginning(1);
+
+            Console.WriteLine("\nLinked List Content:");
+            myDblLinkedList.PrintList();
+
+            Console.WriteLine("\nItem(2) value is: " + myDblLinkedList.GetItem(2));
+
+        }
+    }
+}

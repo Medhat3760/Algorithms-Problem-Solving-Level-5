cc81fd0c7d4ec97985a9948b62846ad9476c2cfa
Medhat3760 <Medhat3760@gmail.com>
2025-10-22 05:06:57 +0200

    Added an extensions to Project 2

---
+++ MyQueue Class Extensions/DS/clsMyQueue.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Double_Linked_List_Class.DS;
+namespace MyQueue_Class.DS
+{
+    public class clsMyQueue<T>
+    {
+
+        protected clsDblLinkedList<T> _myList = new clsDblLinkedList<T>();
+
+        public void Push(T item)
+        {
+            _myList.InsertAtEnd(item);
+        }
+
+        public void Pop()
+        {
+            _myList.DeleteFirstNode();
+        }
+
+        public T Front()
+        {
+            return _myList.GetItem(0);
+        }
+
+        public T Back()
+        {
+            return _myList.GetItem(_myList.Size - 1);
+        }
+
+        public bool IsEmpty()
+        {
+            return _myList.IsEmpty();
+        }
+
+        public int Size()
+        {
+            return _myList.Size;
+        }
+
+        public void Print()
+        {
+            _myList.PrintList();
+        }
+
+        public T GetItem(int index)
+        {
+            return _myList.GetItem(index);
+        }
+
+        public void Reverse()
+        {
+            _myList.Reverse();
+        }
+
+        public bool UpdateItem(int index, T newValue)
+        {
+            return _myList.UpdateItem(index, newValue);
+        }
+
+        public bool InsertAfter(int index, T item)
+        {
+            return _myList.InsertAfter(index, item);
+        }
+
+        public void InsertAtFront(T item)
+        {
+            _myList.InsertAtBeginning(item);
+        }
+
+        public void InsertAtBack(T item)
+        {
+            _myList.InsertAtEnd(item);
+        }
+
+        public void Clear()
+        {
+            _myList.Clear();
+        }
+
+    }
+}
---
+++ MyQueue Class Extensions/Program.cs
@@ -0,0 +1,68 @@
+﻿using MyQueue_Class.DS;
+using System;
+
+namespace MyQueue_Class_Extensions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            clsMyQueue<int> myQueue = new clsMyQueue<int>();
+
+            myQueue.Push(10);
+            myQueue.Push(20);
+            myQueue.Push(30);
+            myQueue.Push(40);
+            myQueue.Push(50);
+
+            Console.WriteLine("\nQueue:");
+            myQueue.Print();
+
+            Console.WriteLine("\nQueue Size: " + myQueue.Size());
+            Console.WriteLine("Queue Front: " + myQueue.Front());
+            Console.WriteLine("Queue Back: " + myQueue.Back());
+
+            myQueue.Pop();
+
+            Console.WriteLine("\nQueue After Pop:");
+            myQueue.Print();
+
+            //Extension #1
+            Console.WriteLine("\n Item(2) : " + myQueue.GetItem(2));
+
+            //Extension #2
+            myQueue.Reverse();
+            Console.WriteLine("\nQueue after Reverse :");
+            myQueue.Print();
+
+            //Extension #3
+            myQueue.UpdateItem(2, 600);
+            Console.WriteLine("\nQueue after updating Item(2) to 600:");
+            myQueue.Print();
+
+            //Extension #4
+            myQueue.InsertAfter(2, 800);
+            Console.WriteLine("\nQueue after Inserting 800 after Item(2):");
+            myQueue.Print();
+
+            //Extension #5
+            myQueue.InsertAtFront(1000);
+            Console.WriteLine("\nQueue after Inserting 1000 at front:");
+            myQueue.Print();
+
+            //Extension #6
+            myQueue.InsertAtBack(2000);
+            Console.WriteLine("\nQueue after Inserting 2000 at back:");
+            myQueue.Print();
+
+            //Extension #7
+            myQueue.Clear();
+            Console.WriteLine("\nQueue after Clear:");
+            myQueue.Print();
+
+            Console.ReadKey();
+
+        }
+    }
+}

e54506fd7d0eb1f2a40548009fa55455944f442f
Medhat3760 <Medhat3760@gmail.com>
2025-10-22 02:28:40 +0200

    Added a Project 2

---
+++ MyQueue Class/DS/clsMyQueue.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Double_Linked_List_Class.DS;
+namespace MyQueue_Class.DS
+{
+    public class clsMyQueue<T>
+    {
+
+        protected clsDblLinkedList<T> _myList = new clsDblLinkedList<T>();
+
+        public void Push(T item)
+        {
+            _myList.InsertAtEnd(item);
+        }
+
+        public void Pop()
+        {
+            _myList.DeleteFirstNode();
+        }
+
+        public T Front()
+        {
+            return _myList.GetItem(0);
+        }
+
+        public T Back()
+        {
+            return _myList.GetItem(_myList.Size - 1);
+        }
+
+        public bool IsEmpty()
+        {
+            return _myList.IsEmpty();
+        }
+
+        public int Size()
+        {
+            return _myList.Size;
+        }
+
+        public void Print()
+        {
+            _myList.PrintList();
+        }
+
+    }
+}
---
+++ MyQueue Class/Program.cs
@@ -0,0 +1,31 @@
+﻿using MyQueue_Class.DS;
+namespace MyQueue_Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            clsMyQueue<int> myQueue = new clsMyQueue<int>();
+
+            myQueue.Push(10);
+            myQueue.Push(20);
+            myQueue.Push(30);
+            myQueue.Push(40);
+            myQueue.Push(50);
+
+            Console.WriteLine("\nQueue:");
+            myQueue.Print();
+
+            Console.WriteLine("\nQueue Size: " + myQueue.Size());
+            Console.WriteLine("Queue Front: " + myQueue.Front());
+            Console.WriteLine("Queue Back: " + myQueue.Back());
+
+            myQueue.Pop();
+
+            Console.WriteLine("\nQueue After Pop:");
+            myQueue.Print();
+
+        }
+    }
+}

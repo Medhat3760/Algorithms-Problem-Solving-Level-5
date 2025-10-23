2e3c84b40445fa2b97b567e0d6013a9b2899f989
Medhat3760 <Medhat3760@gmail.com>
2025-10-23 05:23:12 +0200

    Added a Project 4

---
+++ MyDynamicArray/Program.cs
@@ -0,0 +1,25 @@
+﻿using MyDynamicArray.DS;
+namespace MyDynamicArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            clsDynamicArray<int> myDynamicArray = new clsDynamicArray<int>(10);
+
+            myDynamicArray.SetItem(0, 10);
+            myDynamicArray.SetItem(1, 20);
+            myDynamicArray.SetItem(2, 30);
+            myDynamicArray.SetItem(3, 40);
+            myDynamicArray.SetItem(4, 50);
+
+            Console.WriteLine("\nIs Empty? " + myDynamicArray.IsEmpty());
+            Console.WriteLine("Array Size : " + myDynamicArray.Size);
+
+            Console.WriteLine("\nArray Items:");
+            myDynamicArray.PrintArray();
+
+        }
+    }
+}

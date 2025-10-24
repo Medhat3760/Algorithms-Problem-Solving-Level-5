2076aac5990dd79398307378426b4b1992f7493c
Medhat3760 <Medhat3760@gmail.com>
2025-10-24 07:14:23 +0200

    Added an extension to Project 4

---
+++ MyDynamicArray Extension 03/DS/clsDynamicArray.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace MyDynamicArray.DS
+{
+    public class clsDynamicArray<T>
+    {
+
+        protected int _size;
+        protected T[] _tempArray;
+
+        public T[] originalArray;
+
+        public clsDynamicArray(int size = 0)
+        {
+
+            if (size < 0)
+            {
+                size = 0;
+            }
+
+            _size = size;
+            originalArray = new T[_size];
+
+        }
+
+        public bool SetItem(int index, T value)
+        {
+
+            if (index < 0 || index >= _size)
+            {
+                return false;
+            }
+
+            originalArray[index] = value;
+            return true;
+
+        }
+
+        public bool IsEmpty()
+        {
+            return _size == 0;
+        }
+
+        public int Size
+        {
+            get
+            {
+                return _size;
+            }
+        }
+
+        public void PrintArray()
+        {
+
+            foreach (T item in originalArray)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+        }
+
+        public void Resize(int newSize)
+        {
+
+            if (newSize < 0)
+            {
+                newSize = 0;
+            }
+
+            _tempArray = new T[newSize];
+
+            if (newSize < _size)
+            {
+                _size = newSize;
+            }
+
+            for (int i = 0; i < _size; i++)
+            {
+                _tempArray[i] = originalArray[i];
+            }
+
+            _size = newSize;
+
+            originalArray = _tempArray;
+
+        }
+
+        public T GetItem(int index)
+        {
+
+            if (index < 0 || index >= _size)
+            {
+                throw new IndexOutOfRangeException("out of range");
+            }
+
+            return originalArray[index];
+        }
+
+        public void Reverse()
+        {
+
+            _tempArray = new T[_size];
+
+            for (int i = 0; i < _size; i++)
+            {
+
+                _tempArray[i] = originalArray[_size - i - 1];
+
+            }
+
+            originalArray = _tempArray;
+
+        }
+
+        public void Clear()
+        {
+
+            _size = 0;
+            _tempArray = new T[0];
+            originalArray = _tempArray;
+
+        }
+
+        //My Solution
+        /*public bool DeleteItemAt(int index)
+        {
+
+            if (index < 0 || index >= _size)
+            {
+                return false;
+            }
+
+            _tempArray = new T[_size - 1];
+
+            int counter = 0;
+            for (int i = 0; i < _size; i++)
+            {
+
+                if (i != index)
+                {
+                    _tempArray[counter++] = originalArray[i];
+                }
+
+            }
+
+            _size--;
+
+            originalArray = _tempArray;
+
+            return true;
+
+        }
+        */
+
+        public bool DeleteItemAt(int index)
+        {
+
+            if (index < 0 || index >= _size)
+            {
+                return false;
+            }
+
+            _tempArray = new T[_size-1];
+
+            //copy all before index
+            for (int i = 0; i < index; i++)
+            {
+                _tempArray[i] = originalArray[i];
+            }
+
+            //copy all after index
+            for (int i = index + 1; i < _size; i++)
+            {
+                _tempArray[i - 1] = originalArray[i];
+            }
+
+            _size--;
+
+            originalArray = _tempArray;
+
+            return true;
+
+        }
+
+    }
+}
---
+++ MyDynamicArray Extension 03/Program.cs
@@ -0,0 +1,33 @@
+﻿using MyDynamicArray.DS;
+namespace MyDynamicArray_Extension_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            clsDynamicArray<int> myDynamicArray = new clsDynamicArray<int>(5);
+
+            myDynamicArray.SetItem(0, 10);
+            myDynamicArray.SetItem(1, 20);
+            myDynamicArray.SetItem(2, 30);
+            myDynamicArray.SetItem(3, 40);
+            myDynamicArray.SetItem(4, 50);
+
+            Console.WriteLine("\nIs Empty? " + myDynamicArray.IsEmpty());
+            Console.WriteLine("Array Size : " + myDynamicArray.Size);
+
+            Console.WriteLine("\nArray Items:");
+            myDynamicArray.PrintArray();
+
+            myDynamicArray.DeleteItemAt(2);
+
+            Console.WriteLine("\nArray items after deleting item(2):");
+            Console.WriteLine("\nArray size : " + myDynamicArray.Size);
+            myDynamicArray.PrintArray();
+
+            Console.ReadKey();
+
+        }
+    }
+}

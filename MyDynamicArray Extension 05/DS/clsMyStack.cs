73e5db3ceebc101044f139c36261e798a9496110
Medhat3760 <Medhat3760@gmail.com>
2025-10-26 09:01:37 +0200

    Added extensions to Project 4

---
+++ MyDynamicArray Extension 05/DS/clsMyStack.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyQueue_Class.DS;
+namespace MyStack.DS
+{
+    public class clsMyStack<T> : clsMyQueue<T>
+    {
+
+        public void Push(T item)
+        {
+            _myList.InsertAtBeginning(item);
+        }
+
+        public T Top()
+        {
+            return Front();
+        }
+
+        public T Bottom()
+        {
+            return Back();
+        }
+
+    }
+}
---
+++ MyDynamicArray Extension 05/Program.cs
@@ -0,0 +1,46 @@
+﻿using MyDynamicArray.DS;
+
+namespace MyDynamicArray_Extension_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            clsDynamicArray<int> myDynamicArray = new clsDynamicArray<int>(5);
+
+            myDynamicArray.SetItem(0, 10);
+            myDynamicArray.SetItem(1, 20);
+            myDynamicArray.SetItem(2, 30);
+            myDynamicArray.SetItem(3, 40);
+            myDynamicArray.SetItem(4, 50);
+
+            Console.WriteLine("\nIs Empty? " + myDynamicArray.IsEmpty());
+            Console.WriteLine("Array Size : " + myDynamicArray.Size);
+
+            Console.WriteLine("\nArray Items:");
+            myDynamicArray.PrintArray();
+
+            int index = myDynamicArray.Find(30);
+
+            if(index == -1)
+            {
+                Console.WriteLine("\nItem is NOT found :-(");
+            }
+            else
+            {
+                Console.WriteLine("\n30 is found at index: " + index);
+            }
+
+            myDynamicArray.DeleteItem(30);
+
+            Console.WriteLine("\nArray Items after deleting 30:");
+            Console.WriteLine("Array Size : " + myDynamicArray.Size);
+            myDynamicArray.PrintArray();
+
+
+            Console.ReadKey();
+
+        }
+    }
+}

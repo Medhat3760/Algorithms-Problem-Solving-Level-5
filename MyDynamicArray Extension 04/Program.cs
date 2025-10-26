2050a91983ffd2024516cfab4a9f168e4f201089
Medhat3760 <Medhat3760@gmail.com>
2025-10-26 06:35:21 +0200

    Added extensions to Project 4

---
+++ MyDynamicArray Extension 04/Program.cs
@@ -0,0 +1,42 @@
+﻿using MyDynamicArray.DS;
+namespace MyDynamicArray_Extension_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            clsDynamicArray<int> myDynamicArray = new clsDynamicArray<int>(5);
+
+            myDynamicArray.SetItem(0, 10);
+            myDynamicArray.SetItem(1, 20);
+            myDynamicArray.SetItem(2, 30);
+            myDynamicArray.SetItem(3, 40);
+            myDynamicArray.SetItem(4, 50);
+
+            Console.WriteLine("\nIs Empty? " + myDynamicArray.IsEmpty());
+            Console.WriteLine("Array Size : " + myDynamicArray.Size);
+
+            Console.WriteLine("\nArray Items:");
+            myDynamicArray.PrintArray();
+
+            myDynamicArray.DeleteFirstItem();
+
+            Console.WriteLine("\nArray Items After delete First Item:");
+            Console.WriteLine("\nArray Size : " + myDynamicArray.Size);
+            myDynamicArray.PrintArray();
+
+            myDynamicArray.DeleteLastItem();
+            
+            Console.WriteLine("\nArray Items After delete Last Item:");
+            Console.WriteLine("\nArray Size : " + myDynamicArray.Size);
+            myDynamicArray.PrintArray();
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}

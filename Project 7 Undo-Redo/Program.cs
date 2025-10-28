34453a124666276f3109f6d9bbe63dd10d6bb0fa
Medhat3760 <Medhat3760@gmail.com>
2025-10-28 10:22:57 +0200

    Added a Project 7

---
+++ Project 7 Undo-Redo/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Project_7_Undo_Redo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("\n\n\t\t\t\t\t\t Undo/Redo Project\n");
+
+            clsMyString s1 = new clsMyString();
+
+            Console.WriteLine("\nS1 = " + s1.Value);
+
+            s1.Value = "Abdelrahman";
+            Console.WriteLine("S1 = " + s1.Value);
+            
+            s1.Value = "Abdelrahman2";
+            Console.WriteLine("S1 = " + s1.Value);
+            
+            s1.Value = "Abdelrahman3";
+            Console.WriteLine("S1 = " + s1.Value);
+
+            Console.WriteLine("\n________________");
+
+            s1.Undo();
+            Console.WriteLine("\nS1 after undo = " + s1.Value);
+
+            s1.Undo();
+            Console.WriteLine("S1 after undo = " + s1.Value);
+            
+            s1.Undo();
+            Console.WriteLine("S1 after undo = " + s1.Value);
+
+            Console.WriteLine("\n________________");
+
+            s1.Redo();
+            Console.WriteLine("\nS1 after Redo = " + s1.Value);
+
+            s1.Redo();
+            Console.WriteLine("S1 after Redo = " + s1.Value);
+
+            s1.Redo();
+            Console.WriteLine("S1 after Redo = " + s1.Value);
+
+        }
+    }
+}
---
+++ Project 7 Undo-Redo/clsMyString.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_7_Undo_Redo
+{
+    public class clsMyString
+    {
+
+        private string _value = string.Empty;
+
+        private Stack<string> _undo = new Stack<string>();
+        private Stack<string> _redo = new Stack<string>();
+
+        public string Value
+        {
+
+            get
+            {
+                return _value; 
+            }
+
+            set
+            {
+
+                _undo.Push(_value);
+                _value = value;
+                _redo.Clear();
+
+            }
+
+        }
+
+        public void Undo()
+        {
+
+            if (_undo.Count >0)
+            {
+                _redo.Push(_value);
+                _value = _undo.Pop();
+            }
+
+        }
+        
+        public void Redo()
+        {
+
+            if (_redo.Count >0)
+            {
+                _undo.Push(_value);
+                _value = _redo.Pop();
+            }
+            
+        }
+
+    }
+}

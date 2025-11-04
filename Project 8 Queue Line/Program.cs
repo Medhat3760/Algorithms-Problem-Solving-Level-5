cd6e6c3645054d325280980b693baf1f503ff748
Medhat3760 <Medhat3760@gmail.com>
2025-11-04 15:22:01 +0200

    Added Project 8

---
+++ Project 8 Queue Line/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Project_8_Queue_Line
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            clsQueueLine payBillsQueue = new clsQueueLine("A0", 10);
+            clsQueueLine subscriptionsQueue = new clsQueueLine("B0", 5);
+
+            payBillsQueue.IssueTicket();
+            payBillsQueue.IssueTicket();
+            payBillsQueue.IssueTicket();
+            payBillsQueue.IssueTicket();
+            payBillsQueue.IssueTicket();
+
+            Console.WriteLine("\nPay Bills Queue Info:");
+            payBillsQueue.PrintInfo();
+
+            payBillsQueue.PrintTicketsLineRTL();
+            payBillsQueue.PrintTicketsLineLTR();
+
+            payBillsQueue.PrintAllTickets();
+
+            payBillsQueue.ServeNextClient();
+
+            Console.WriteLine("\nPay Bills Queue After serving one client:");
+            payBillsQueue.PrintInfo();
+
+
+
+            Console.WriteLine("\nSubscriptions Queue Info:");
+
+            subscriptionsQueue.IssueTicket();
+            subscriptionsQueue.IssueTicket();
+            subscriptionsQueue.IssueTicket();
+
+            subscriptionsQueue.PrintInfo();
+
+            subscriptionsQueue.PrintTicketsLineRTL();
+            subscriptionsQueue.PrintTicketsLineLTR();
+
+            subscriptionsQueue.PrintAllTickets();
+
+            subscriptionsQueue.ServeNextClient();
+
+            Console.WriteLine("\nSubsriptions Queue After serving one client:");
+            subscriptionsQueue.PrintInfo();
+
+        }
+    }
+}
---
+++ Project 8 Queue Line/clsQueueLine.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using MyLib;
+
+namespace Project_8_Queue_Line
+{
+    public class clsQueueLine
+    {
+
+        private short _totalTickets = 0;
+        private string _prefix = "";
+        private short _averageServeTime = 0;
+
+        class clsTicket
+        {
+
+            private string _prefix;
+            private short _number = 0;
+            private int _watingClients = 0;
+            private string _ticketTime = "";
+            private short _averageServeTime = 0;
+            private short _expectedServeTime = 0;
+
+            public clsTicket(string prefix, short number, int waitingClients, short averageServeTime)
+            {
+                _prefix = prefix;
+                _number = number;
+                _watingClients = waitingClients;
+                _ticketTime = clsDate.GetSystemDateTimeString();
+                _averageServeTime = averageServeTime;
+            }
+
+            public string Prefix
+            {
+                get
+                {
+                    return _prefix;
+                }
+            }
+            
+            public short Number
+            {
+                get
+                {
+                    return _number;
+                }
+            }
+
+            public string FullNumber()
+            {
+                return _prefix + _number.ToString();
+            }
+
+            public int WatingClients => _watingClients;
+
+            public string TicketTime => _ticketTime;
+
+
+            public short ExpectedServeTime
+            {
+
+                get
+                {
+                    _expectedServeTime = (short)(_averageServeTime * _watingClients);
+                    return _expectedServeTime;
+                }
+
+            }
+
+            public void Print()
+            {
+
+                Console.WriteLine("\t\t\t\t ___________________________________");
+                Console.WriteLine("\n\t\t\t\t\t\t" + FullNumber());
+                Console.WriteLine("\n\t\t\t\t\t" + TicketTime);
+                Console.WriteLine("\t\t\t\t\tWating Clients = " + _watingClients);
+                Console.WriteLine("\t\t\t\t\t  Serve Time In");
+                Console.WriteLine("\t\t\t\t\t    " + ExpectedServeTime.ToString() + " Minutes.");
+                Console.WriteLine("\t\t\t\t ___________________________________\n");
+
+            }
+
+        }
+
+        private Queue<clsTicket> _queueLine = new Queue<clsTicket>();
+
+        public clsQueueLine(string prefix, short averageServeTime)
+        {
+
+            _prefix = prefix;
+            _averageServeTime = averageServeTime;
+            _totalTickets = 0;
+
+        }
+
+        public void IssueTicket()
+        {
+
+            _totalTickets++;
+
+            clsTicket ticket = new clsTicket(_prefix, _totalTickets, WaitingClients(), _averageServeTime);
+
+            _queueLine.Enqueue(ticket);
+
+
+        }
+
+        public int WaitingClients()
+        {
+
+            return _queueLine.Count;
+
+        }
+
+        public string WhoIsNext()
+        {
+
+            if(_queueLine.Count == 0)
+            {
+                return "No Clients Left.";
+            }
+            else
+            {
+                return _queueLine.Peek().FullNumber();
+            }
+
+        }
+
+        public bool ServeNextClient()
+        {
+
+            if(_queueLine.Count == 0)
+            {
+                return false;
+            }
+
+            _queueLine.Dequeue();
+            return true;
+
+        }
+
+        public short ServedClients()
+        {
+
+            return (short)(_totalTickets - WaitingClients());
+
+        }
+
+        public void PrintInfo()
+        {
+
+            Console.WriteLine("\n\t\t\t\t___________________________________");
+            Console.WriteLine("\n\t\t\t\t\t    Queue Info");
+            Console.WriteLine("\t\t\t\t___________________________________");
+
+            Console.WriteLine("\n\t\t\t\t\tPrefix         = " + _prefix);
+            Console.WriteLine("\t\t\t\t\tTotal Tickets  = " + _totalTickets);
+            Console.WriteLine("\t\t\t\t\tServed Clients = " + ServedClients());
+            Console.WriteLine("\t\t\t\t\tWating Clients = " + WaitingClients());
+
+            Console.WriteLine("\t\t\t\t___________________________________");
+
+        }
+
+        public void PrintTicketsLineRTL()
+        {
+
+            if(_queueLine.Count == 0)
+            {
+
+                Console.WriteLine("\n\n\t\t\tTickets: No Tickets.");
+                return;
+
+            }
+            else
+            {
+                Console.Write("\n\n\t\t\t Tickets:  ");
+            }
+
+            foreach(clsTicket ticket in _queueLine)
+            {
+                Console.Write(" "+ticket.FullNumber() + " <-- ");
+            }
+
+            Console.WriteLine();
+
+        }
+        
+        public void PrintTicketsLineLTR()
+        {
+
+            if (_queueLine.Count == 0)
+            {
+
+                Console.WriteLine("\n\t\t\t Tickets: No Tickets.");
+                return;
+
+            }
+            else
+            {
+                Console.Write("\n\t\t\t Tickets:  ");
+            }
+
+            Stack<clsTicket> stack = new Stack<clsTicket>(_queueLine);
+
+            foreach(clsTicket ticket in stack)
+            {
+                Console.Write(" " + ticket.FullNumber() + " --> ");
+            }
+
+            Console.WriteLine();
+
+        }
+
+        public void PrintAllTickets()
+        {
+
+            Console.WriteLine("\n\n\t\t\t\t\t     --Tickets--");
+
+            if (_queueLine.Count == 0)
+            {
+                Console.WriteLine("\n\n\t\t\t     ---No Tickets---\n");
+                return;
+            }
+
+            foreach (clsTicket ticket in _queueLine)
+            {
+                ticket.Print();
+            }
+
+        }
+
+    }
+}
